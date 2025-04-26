3c1bbaa62f04d754ad7314a0fff3ab1b3567abd9
Khushbu Thakur <thakur.khushbu@anudip.org>
2025-04-26 16:25:01 +0530

    first commit

---
+++ lab3practical1/src/main/java/com/anudip/lab3practical1/App.java
@@ -0,0 +1,30 @@
+package com.anudip.lab3practical1;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        // Locate search box using Name locator
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Selenium WebDriver");
+        searchBox.submit();
+
+  
+
+    }
+}
